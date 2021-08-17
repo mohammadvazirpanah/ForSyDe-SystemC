c7d7fc528771622cf491651e49e7e4bfe1f63bf0
mohammad <mohammad.vazirpanah@yahoo.com>
2021-08-17 18:40:31 +0430

    Last Change

--- examples/ros/controller.hpp
+++ examples/ros/controller.hpp
@@ -7,16 +7,49 @@ using namespace sc_core;
 using namespace ForSyDe;
 
 
-void controller_ns_func(std::vector <double> & ns,
-    const std::vector <double>& cs,
+void controller_ns_func(std::tuple<double,double, double>& ns,
+    const std::tuple<double,double, double>& cs,
     const double& inp)
 {
-    double right_sonar, left_sonar, left2_sonar;
-    
+    double right_sonar, left_sonar, left2_sonar, vx, vy;
+    std::tie(right_sonar, left_sonar, left2_sonar) = cs;
+
+    if (left2_sonar < 1 && left_sonar >= 1 && right_sonar >= 1)
+    {
+        vy = -0.57735026919 * inp; 
+        vx = -1 * inp;
+
+    }
+    else if (left2_sonar >= 1 && left_sonar < 1 && right_sonar >= 1)
+    {
+        vy = inp; 
+        vx = 0;
+    }
+    else if (left2_sonar < 1 && left_sonar >= 1 && right_sonar < 1)
+    {
+        vy = -1 * inp;
+        vx = 0;
+    }
+    else if (left2_sonar < 1 && left_sonar < 1 && right_sonar > 1)
+    {
+        vy = 0.57735026919 * inp; 
+        vx = -1 * inp;
+    }
+    else if (left2_sonar >= 1 && left_sonar >= 1 && right_sonar < 1)
+    {
+        vy = -0.57735026919 * inp; 
+        vx = inp;
+    }
+    else if (left2_sonar >= 1 && left_sonar < 1 && right_sonar < 1)
+    {
+        vy = -0.57735026919 * inp; 
+        vx = -1 * inp;
+    }
+
 }
 
 void controller_od_func(double& out,
-    const std::vector <double>& cs,
+    const std::tuple<double,double, double>& cs,
     const double& inp)
 {
    
--- examples/ros/top.hpp
+++ examples/ros/top.hpp
@@ -13,25 +13,29 @@ using namespace ForSyDe;
 
 SC_MODULE(top)
 {
-    SY::signal<int> srca, result;
+    SY::signal<double> srca, result;
     
     SC_CTOR(top)
     {
     
-        std::cout<<"SC_CTOR \n";
+        std::cout<<"SC_CTOR Start \n";
 
         
-        SY::make_ssource("source1", func, 1, 2, srca);
+        //SY::make_ssource("source1", func, 1, 2, srca);
         //SY::roswrap<std::string,std::string> *ros1 = new SY::roswrap<std::string,std::string>("ros1","sender",result, srca);
 
         SY::make_roswrap("roswrap","ForsydeToRos","RosToForsyde", result, srca); // (Module Name, Sender Topic, Recevier Topic, out , in)
-    
-        /*SY::make_mealy("controller1",
-		controller_ns_func,
-		controller_od_func,
-		const ST & init_st 
-	    ) */	
-        SY::make_ssink("report1", report_func, result);
+        
+        SY::make_smealy("controller1",
+                controller_ns_func,
+                controller_od_func,
+                std::make_tuple(0.0, 0.0, 0.0),
+                srca,
+                result
+            );
+
+
+        //SY::make_ssink("report1", report_func, result);
         
         
 
