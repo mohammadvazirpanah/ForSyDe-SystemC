890fc487e3a1284dd3ec091efb9baf039515bd36
mohammad <mohammad.vazirpanah@yahoo.com>
2021-10-01 17:36:11 +0330

    destination_angle

--- examples/ros/func.hpp
+++ examples/ros/func.hpp
@@ -2,15 +2,35 @@
 #define FUNC_HPP
 
 #include <forsyde.hpp>
-
+#include <math.h>
+#define pi 3.14159265359
 using namespace ForSyDe;
 
 
-void func(int& out1, const int& inp1)
+float func(float inp1, float inp2) 
 {
-#pragma ForSyDe begin func
-    out1 = inp1 + 1;
-#pragma ForSyDe end
+// inp1 = destination_x & inp2 = destination_y & out1 = destination_angle 
+
+
+    float raw_angle = atan (inp1/inp2);
+    float out1 = raw_angle;
+
+    if (raw_angle >=0 && (inp1 <0 || inp2 < 0)) 
+        out1 = raw_angle + pi;
+
+    else if (raw_angle < 0 && (inp2 < 0 || inp1 > 0))
+        out1 = raw_angle + pi;
+    
+    out1 = fmod(pi + out1, 2*pi);
+
+    if(abs(out1 - 2*pi) < abs(out1))
+        out1 = abs(out1 - 2*pi);
+
+    if(abs(out1 + 2*pi) < abs(out1))
+        out1 = abs(out1 + 2*pi);
+
+    return out1;
 }
 
 #endif
+
