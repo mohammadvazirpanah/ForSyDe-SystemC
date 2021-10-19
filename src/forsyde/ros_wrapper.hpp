7b27c532e117daad752339be9ada1697929f94f6
mohammad <mohammad.vazirpanah@yahoo.com>
2021-10-19 12:25:33 +0330

    Inverse Senematic Resolved in Callback Function without Testing

--- src/forsyde/ros_wrapper.hpp
+++ src/forsyde/ros_wrapper.hpp
@@ -11,7 +11,7 @@
 #include "sy_process.hpp"
 #include "abst_ext.hpp"
 #include <stdio.h>
-
+#include <math.h>
 
 
 
@@ -32,13 +32,24 @@ public:
     SY_out<T0> oport1; 
     int argc;
     char **argv; 
+    
+    //typedef std::function<void(abst_ext<T0>&,const abst_ext<T1>&)> functype;
 
-roswrap(const sc_module_name& _name, const std::string& topic_name_sender, const std::string& topic_name_receiver) : 
-sy_process(_name), iport1("iport1"), oport1("oport1"), topic_name_sender(topic_name_sender), topic_name_receiver(topic_name_receiver)
+    roswrap(const sc_module_name& _name,                   // Process name 
+            const std::vector <std::string>& topic_publisher,          
+            const std::vector <std::string>& topic_subscriber
+            //const functype& _func_callback
+            ): 
+            sy_process(_name),
+            iport1("iport1"),
+            oport1("oport1"),
+            topic_publisher(topic_publisher),
+            topic_subscriber(topic_subscriber)
+            //_func_callback(_func_callback)                 //Function to be passed for Callback function in ROS 
 
     {
 #ifdef FORSYDE_INTROSPECTION
-        arg_vec.push_back(std::make_tuple("topic_name",topic_name_sender, topic_name_receiver));
+        arg_vec.push_back(std::make_tuple("topic_name",topic_publisher, topic_subscriber));
 #endif
     }
 
@@ -47,49 +58,136 @@ sy_process(_name), iport1("iport1"), oport1("oport1"), topic_name_sender(topic_n
 
 private:
 
-std::string topic_name_sender;
-std::string topic_name_receiver;
+std::vector <std::string> topic_publisher;
+std::vector <std::string> topic_subscriber;
 std::string _name;
 abst_ext<T1>* ival1;
-abst_ext<T0>* oval;
-//T0* oval;
+abst_ext<T0> ovals;
+
+abst_ext<T0>* rosval1;
+abst_ext<T0>* rosval2;
+abst_ext<T0>* rosval3;
+abst_ext<T0>* transformed_y;
+abst_ext<T0>* transformed_x;
+abst_ext<T0>* angle_radian;
+
+//functype _func_callback;
 std::istringstream ival1_str;
 std::istringstream ival2_str;
 
 ros::Publisher pub;          // Publish in Prep Step
-ros::Subscriber sub;        // Subscribe in Prod Step 
-ros::NodeHandle *n;         //  Node Handle For Ros Environment 
+ros::Subscriber sub1,sub2,sub3,sub4;        // Subscribe in Prod Step 
+ros::NodeHandle *n;         // Node Handle For Ros Environment 
+ros::Rate *rate;            // Ros Rate 
 
 
-    void Callback_Sonar(const sensor_msgs::Range::ConstPtr& msg)
+    void Callback_Jonit(const sensor_msgs::JointState::ConstPtr& msg)
     {
-      float max_range = msg-> max_range ;
-      float min_range = msg-> min_range ;
-      float range = msg->range ; 
-      oval->set_val(msg->min_range);
-      ROS_INFO("Sonar Range: [%f]", oval->from_abst_ext(0.0));
+        ROS_INFO("Joint State left_pos: [%f]", -msg->position[0]);
+        ROS_INFO("Joint State left2_pos: [%f]", msg->position[1]);
+        ROS_INFO("Joint State right_pos: [%f]", msg->position[2]);
+
+        double x_pos, y_pos, angular_pos;
+
+        double left_pos =(-1.0) * msg->position[0];
+        double left2_pos = msg->position[1];
+        double right_pos = msg->position[2];
+
+        angular_pos   =  ((-1.0)*(1.0/3.0) * (left2_pos)) + ((-1.0)*(1.0/3.0) * (right_pos)) + ((-1.0)*(1.0/3.0) * (left_pos));
+        x_pos         =  ((2.0/3.0) * (left2_pos)) + ((-1.0)*(1.0/3.0) * (right_pos)) + ((-1.0)*(1.0/3.0) * (left_pos)); 
+        y_pos         =  ( 0.0 * left2_pos) + ((-1.0)*(0.57735026919) * (right_pos)) + (0.57735026919 * (left_pos));
+
+        double cur_x, cur_y, last_in_y, last_in_x, added_y, added_x;
+        if(cur_x == 0)
+        {
+          cur_x = 0.0;
+          cur_y = 0.0;
+          last_in_x = 0.0;
+          last_in_y = 0.0;
+        }
+
+        added_y = (y_pos - last_in_y)*cos((-1)*angular_pos) + (x_pos - last_in_x)*sin((-1)*angular_pos);
+        added_x = -1*(y_pos - last_in_y)*sin(-angular_pos) + (x_pos - last_in_x)*cos((-1)*angular_pos);
+        cur_x = cur_x + added_x;
+        cur_y = cur_y + added_y;
+        last_in_x = x_pos;
+        last_in_y = y_pos;
+        transformed_y ->set_val (cur_y);
+        transformed_x -> set_val (cur_x);
+        double temp = ((2 *3.14159265359 * angular_pos) / (50.84)) + (3.14159265359/3.0) ; 
+        angle_radian ->set_val (temp);
+
+        // ROS_INFO("Joint State x_pos: [%f]", x_pos);
+        // ROS_INFO("Joint State y_pos: [%f]", y_pos);
+        // ROS_INFO("Joint State angular_pos: [%f]",angular_pos);
+        
     }
+
+    void Callback_Sonar_1(const sensor_msgs::Range::ConstPtr& msg)
+    {
+      rosval1->set_val(msg->range);
+      // ROS_INFO("Sonar Range 1: [%f]", rosval1->from_abst_ext(0.0));
+    }
+
+    void Callback_Sonar_2(const sensor_msgs::Range::ConstPtr& msg)
+    {
+      rosval2->set_val(msg->range);
+      // ROS_INFO("Sonar Range 2: [%f]", rosval2->from_abst_ext(0.0));
+    }
+
+    void Callback_Sonar_3(const sensor_msgs::Range::ConstPtr& msg)
+    {
+      rosval3->set_val(msg->range);
+      // ROS_INFO("Sonar Range 3: [%f]", rosval3->from_abst_ext(0.0));
+    }
+  
+  
   
     void init()
     {
 
-      oval = new abst_ext<T0>;
+      //oval = new abst_ext<T0>;
+      //oval = new std::vector<T0>;
       ival1 = new abst_ext<T1>;
+      rosval1 = new abst_ext<T0>;
+      rosval2 = new abst_ext<T0>;
+      rosval3 = new abst_ext<T0>;
+      transformed_y = new abst_ext<T0>; 
+      transformed_x = new abst_ext<T0>; 
+      angle_radian = new abst_ext<T0>; 
+
 		  ros::init(argc, argv, "forsyde");
       ros::start();
       n = new ros::NodeHandle;
+      rate = new ros::Rate(1);
+      
       // ival1_str.str(topic_name_sender);
       // ival2_str.str(topic_name_receiver);
       
 
 
-      sub = n->subscribe ("/sonar_sonar_link_1", 1000, &roswrap::Callback_Sonar, this);
+      sub1 = n->subscribe ("/sonar_sonar_link_1", 1000, &roswrap::Callback_Sonar_1, this);
+      sub2 = n->subscribe ("/sonar_sonar_link_2", 1000, &roswrap::Callback_Sonar_2, this);
+      sub3 = n->subscribe ("/sonar_sonar_link_3", 1000, &roswrap::Callback_Sonar_3, this);
+      sub4 = n->subscribe ("/robot/joint_states", 1000, &roswrap::Callback_Jonit, this);
+
+
       //sub = n.subscribe ("/sonar_sonar_link_2", 1000, Callback_Sonar);
       //sub = n.subscribe ("/sonar_sonar_link_3", 1000, Callback_Sonar);
 
-      pub = n->advertise <std_msgs::Float64> ("robot/left_joint_velocity_controller/command", 1000);
-      //pub = n.advertise <std_msgs::Float64> ("robot/right_joint_velocity_controller/command", 1000); 
-      //pub = n.advertise <std_msgs::Float64> ("robot/left2_joint_velocity_controller/command", 1000); 
+      // for (int i=0 ; i<topic_subscriber.size(); i++)
+      
+        //sub = n->subscribe (topic_subscriber[0], 1000, &roswrap::_func_callback, this);
+        
+      
+      
+      // for (int j=0 ; j<topic_subscriber.size(); j++)
+      
+        //pub = n->advertise <std_msgs::Float64> (topic_publisher[0], 1000);
+      
+
+  
+
 
     }
     
@@ -100,7 +198,7 @@ ros::NodeHandle *n;         //  Node Handle For Ros Environment
       *ival1 = iport1.read();
       std_msgs::Float64 msg ;
       msg.data = ival1->from_abst_ext(0.0);
-      pub.publish(msg);
+      //pub.publish(msg);
       ros::spinOnce(); 
   
       
@@ -111,25 +209,30 @@ ros::NodeHandle *n;         //  Node Handle For Ros Environment
     void prod()
     {
       
-      ros::Rate rate(1);
-      while (oval->is_absent())
+      
+      while (rosval1->is_absent() || rosval2->is_absent() || rosval3->is_absent() || transformed_y->is_absent() || transformed_x->is_absent()  || angle_radian->is_absent())
 
       {
         ros::spinOnce(); 
-        rate.sleep();
+        rate->sleep();
         wait(SC_ZERO_TIME);
       }
 
-      WRITE_MULTIPORT(oport1, abst_ext<T0>(*oval))
-      oval->set_abst();
-
+      WRITE_MULTIPORT(oport1, ovals)
+      rosval1->set_abst();
+      rosval2->set_abst();
+      rosval3->set_abst();
+      transformed_y ->set_abst(); 
+      transformed_x ->set_abst();
+      angle_radian-> set_abst();
     }
     
     void clean()
     {
       delete ival1;
-      delete oval;
+      //delete oval;
       delete n;
+      delete rate;
       ros::shutdown();
     }
 
@@ -142,8 +245,9 @@ ros::NodeHandle *n;         //  Node Handle For Ros Environment
 template <class T0, template <class> class OIf,
           class T1, template <class> class I1If>
 inline roswrap<T0,T1>* make_roswrap(const std::string& pName,
-    const std::string& sName,
-    const std::string& rName,
+    const std::vector <std::string>& sName,
+    const std::vector <std::string>& rName,
+    //const typename roswrap<T0,T1>::functype& _func_callback,
     OIf<T0>& outS,
     I1If<T1>& inp1S
     )
@@ -163,3 +267,13 @@ inline roswrap<T0,T1>* make_roswrap(const std::string& pName,
 }
 
 #endif
+
+
+    // void Callback_Sonar(const sensor_msgs::Range::ConstPtr& msg)
+    // {
+    //   float max_range = msg-> max_range ;
+    //   float min_range = msg-> min_range ;
+    //   float range = msg->range ; 
+    //   oval->set_val(msg->range);
+    //   ROS_INFO("Sonar Range: [%f]", oval->from_abst_ext(0.0));
+    // }
